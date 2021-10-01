fd59327a9fac3db0b3a296a9e740360febfd1eb2
StevenGe <StevenGe@gemi.io>
2021-10-01 14:03:50 +0700

    test read / write excel

--- test_read_excel/src/main/java/ReadExcel.java
+++ test_read_excel/src/main/java/ReadExcel.java
@@ -1,3 +1,109 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFFont;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ReadExcel {
-    
+    public static void main(String[] args){
+        System.out.print("tart");
+
+        readExcel();
+        //writeExcel();
+
+
+    }
+
+    public static void readExcel() {
+        try {
+            String fileLocation = "/Users/gemi/Documents/GitHub/git_st_research/test_read_excel/src/main/resources/temp.xlsx";
+            FileInputStream file = new FileInputStream(new File(fileLocation));
+
+            Workbook workbook = new XSSFWorkbook(file);
+
+            Sheet sheet = workbook.getSheetAt(0);
+
+            Map<Integer, List<String>> data = new HashMap<>();
+            int i = 0;
+            for (Row row : sheet) {
+                data.put(i, new ArrayList<String>());
+                for (Cell cell : row) {
+                    /*
+                    switch (cell.getCellTypeEnum()) {
+                        case STRING: ... break;
+                        case NUMERIC: ... break;
+                        case BOOLEAN: ... break;
+                        case FORMULA: ... break;
+                        default: data.get(new Integer(i)).add(" ");
+                    }*/
+
+                    System.out.println(cell.toString());
+                }
+                i++;
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeExcel() {
+        Workbook workbook = new XSSFWorkbook();
+
+        Sheet sheet = workbook.createSheet("Persons");
+        sheet.setColumnWidth(0, 6000);
+        sheet.setColumnWidth(1, 4000);
+
+        Row header = sheet.createRow(0);
+
+        CellStyle headerStyle = workbook.createCellStyle();
+        headerStyle.setFillForegroundColor(IndexedColors.LIGHT_BLUE.getIndex());
+        headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+
+        XSSFFont font = ((XSSFWorkbook) workbook).createFont();
+        font.setFontName("Arial");
+        font.setFontHeightInPoints((short) 16);
+        font.setBold(true);
+        headerStyle.setFont(font);
+
+        Cell headerCell = header.createCell(0);
+        headerCell.setCellValue("Name");
+        headerCell.setCellStyle(headerStyle);
+
+        headerCell = header.createCell(1);
+        headerCell.setCellValue("Age");
+        headerCell.setCellStyle(headerStyle);
+
+        CellStyle style = workbook.createCellStyle();
+        style.setWrapText(true);
+
+        Row row = sheet.createRow(2);
+        Cell cell = row.createCell(0);
+        cell.setCellValue("John Smith");
+        cell.setCellStyle(style);
+
+        cell = row.createCell(1);
+        cell.setCellValue(20);
+        cell.setCellStyle(style);
+
+        File currDir = new File(".");
+        String path = currDir.getAbsolutePath();
+        String fileLocation = path.substring(0, path.length() - 1) + "temp.xlsx";
+
+        FileOutputStream outputStream = null;
+        try {
+            outputStream = new FileOutputStream(fileLocation);
+            workbook.write(outputStream);
+            workbook.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
